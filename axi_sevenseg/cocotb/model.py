15c43be81d298369ddca727731301c15c257ed10
Stefan Wallentowitz <stefan.wallentowitz@hm.edu>
2019-11-14 15:33:22 +0100

    Comments in cocotb

--- axi_sevenseg/cocotb/model.py
+++ axi_sevenseg/cocotb/model.py
@@ -2,10 +2,14 @@ import cocotb
 from cocotb.result import TestFailure
 from cocotb.triggers import Timer, RisingEdge
 
+# Lookup between digit selection signal and digit index
 an_lookup = {127: 7, 191: 6, 223: 5, 239: 4, 247: 3, 251: 2, 253: 1, 254: 0}
+# Lookup between seven segment and the digit
 ca_lookup = {64: 0, 121: 1, 36: 2, 48: 3, 25: 4, 18: 5, 2: 6, 120: 7, 0: 8,
              24: 9, 8: 10, 3: 11, 70: 12, 33: 13, 6: 14, 14: 15}
 
+# The model tracks to output of the DUT and checks if it matches
+# the expected value (as set externally)
 class SevenSegment(object):
     def __init__(self, dut):
         self.dut = dut
@@ -17,7 +21,9 @@ class SevenSegment(object):
     @cocotb.coroutine
     def monitor(self):
         while True:
+            # Verify on each rising clock edge
             yield RisingEdge(self.dut.i_wb_clk)
+            # The bit paaterns need to be translated
             an = self.dut.o_an.value.integer
             ca = self.dut.o_ca.value.integer
             if an not in an_lookup.keys():
@@ -30,10 +36,12 @@ class SevenSegment(object):
                 raise TestFailure()
             ca = ca_lookup[ca]
 
+            # Extract expected digit
             expected = (self.value >> an*4) & 0xf
 
             if ca != expected:
                 self.dut._log.error("Expected {}, got {}".format(expected, ca))
                 raise TestFailure()
 
+            # Count up how often each index was seen
             self.observations[an] += 1
--- axi_sevenseg/cocotb/test_wb_sevenseg.py
+++ axi_sevenseg/cocotb/test_wb_sevenseg.py
@@ -1,20 +1,25 @@
 import random
 
 import cocotb
-from cocotb.result import TestFailure
-from cocotb.triggers import Timer, RisingEdge, FallingEdge
+from cocotb.triggers import Timer, RisingEdge
 from cocotb.clock import Clock
 
 from model import SevenSegment
 
+# This is the co-routine which configures the DUT over
+# the bus interface with a given value
 @cocotb.coroutine
 def configure(dut, value):
+    # wait until after next rising clock edge
     yield RisingEdge(dut.i_wb_clk)
+    # Set pins
     dut.i_wb_sel <= 0xf
     dut.i_wb_we <= 1
     dut.i_wb_cyc <= 1
     dut.i_wb_stb <= 1
     dut.i_wb_dat <= value
+    # wait until the next clock edge and check
+    # ack until we see it
     yield RisingEdge(dut.i_wb_clk)
     while dut.o_wb_ack.value == 0:
         yield RisingEdge(dut.i_wb_clk)
@@ -22,31 +27,43 @@ def configure(dut, value):
     dut.i_wb_stb <= 0
 
 
+# This is the actual test that is picked up
 @cocotb.test()
 def sevenseg_test(dut, tests=20):
     dut.i_wb_cyc = 0
     dut.i_wb_stb = 0
+    # Clock
     c = Clock(dut.i_wb_clk, 2)
     cocotb.fork(c.start())
+
+    # Reset cycle
     dut.i_wb_rst = 1
     yield Timer(10)
     dut.i_wb_rst = 0
 
+    # Instantiate model of seven segment
     model = SevenSegment(dut)
+
+    # Initialize model and DUT to same state
     yield configure(dut, 0)
     model.value = 0
     yield Timer(10)
 
+    # Let model monitor DUT output
     monitor = cocotb.fork(model.monitor())
 
+    # Run given number of tests
     for i in range(tests):
+        # Wait states
         yield Timer(random.randint(1,20))
+        # Random value to display
         v = random.getrandbits(32)
         yield configure(dut, v)
         yield RisingEdge(dut.i_wb_clk)
         model.value = v
 
+    # Terminatre monitor
     monitor.kill()
 
+    # Display how often each digit has been seen
     dut._log.info("observations: {}".format(model.observations))
-
